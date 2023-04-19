757d798ce6f616be752ba8eae7adc01be9410e50
ᴋʀɪsʜsᴜᴘᴇʀʏᴏ <131173834+Krishsuperyo@users.noreply.github.com>
2023-04-19 15:17:41 +0530

    Create __init__.py

---
+++ Main/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2023-present by Team-Asterix@Github, < https://github.com/Asterix >.
+#
+# This file is part of < https://github.com/Team-Asterix/Asterix > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Asterix/Asterix/blob/main/LICENSE >
+#
+# All rights reserved.
+
+from pyromod import listen
+from .core.client import AltruixClient
+
+
+Altruix = AltruixClient()
