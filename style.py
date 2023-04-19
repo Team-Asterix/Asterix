89b329af62ae50e182f8de2252973d320742949b
ᴋʀɪsʜsᴜᴘᴇʀʏᴏ <131173834+Krishsuperyo@users.noreply.github.com>
2023-04-19 14:52:05 +0530

    Create style.py

---
+++ style.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2023-present by Team-Asterix@Github, < https://github.com/Team-Asterix >.
+#
+# This file is part of < https://github.com/Team-Asterix/Asterix > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Asterix/Asterix/blob/main/LICENSE >
+#
+# All rights reserved.
+
+# Edit this file to customize style of your UserBot.
+# Works only well with local deployment and github forks, .env based
+# solution OTW
+
+bullets = {
+    "bullet1": ">",
+    "bullet2": "•",
+    "bullet3": "⋟",
+    "bullet4": "◈",
+    "bullet5": "┏",
+    "bullet6": "┣",
+    "bullet7": "┗",
+}  # used in .ls - cloud_tools.py, .ci & .ui - chat_utils.py and in .listsudo - sudo_manager.py
+
+alive_format = {
+    # tbd
+}  # used in .alive - alive.py
+
+ping_format = {
+    "ping_emoji1": "🏓",
+    "ping_emoji2": "⚙️",
+}  # used in .ping - ping.py UserBot & Bot and in inline restart
+
+def_emoji = {"emoji": "😁"}  # used in .kang - stcr.py
