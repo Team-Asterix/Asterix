bbc774bc6fdc849029594442b027c65196f2dcf3
ᴋʀɪsʜsᴜᴘᴇʀʏᴏ <131173834+Krishsuperyo@users.noreply.github.com>
2023-04-19 14:59:51 +0530

    Create __main__.py

---
+++ tests/__main__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2023-present by Team-Asterix@Github, < https://github.com/Team-Asterix >.
+#
+# This file is part of < https://github.com/Team-Asterix/Asterix > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Asterix/Asterix/blob/main/LICENSE >
+#
+# All rights reserved.
+
+from Main import Altruix
+
+
+if __name__ == "__main__":
+    Altruix.test_run()
