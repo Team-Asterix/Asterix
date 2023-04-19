93314be64d244f0818f853fd77ff5923d8ad58b2
ᴋʀɪsʜsᴜᴘᴇʀʏᴏ <131173834+Krishsuperyo@users.noreply.github.com>
2023-04-19 15:01:35 +0530

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2023-present by Team-Asterix@Github, < https://github.com/Team-Asterix >.
+#
+# This file is part of < https://github.com/Team-Asterix/Asterix > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Asterix/Asterix/blob/main/LICENSE >
+#
+# All rights reserved.
